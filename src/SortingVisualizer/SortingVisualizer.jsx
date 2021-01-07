5663d30407180303036255755348f89037e5bd36
Varun Kedia <purplevarun@gmail.com>
2021-01-07 12:44:45 +0530

    merge sort naive

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css';
+import * as sortingAlgorithms from '../sortingAlgorithms/sortingAlgorithms';
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
         super(props);
@@ -13,14 +14,34 @@ export default class SortingVisualizer extends React.Component {
     }
     shuffleArray(){
         const a =[]; // empty array
-        for(let i=0;i<75;i++){
-            let value = Math.random()*100 + 10;
+        for(let i=0;i<200;i++){
+            let value = Math.random()*100 + 1;
             value = Math.floor(value);   
             a.push(value);
         }
-        // console.log(a);
+        console.log(a);
         this.setState({a});
     }
+    // sorting algorithms placed here
+    mergeSort(){
+        const jsarray = this.state.a.slice().sort((a,b)=>(a-b));
+        // alert("merge sort called");
+        const sortedArray = sortingAlgorithms.mergeSort(this.state.a);
+        // console.log("js sorted array = "+jsarray);
+        console.log(checkarrays(jsarray,sortedArray));
+        // console.log("merge sorted array = " + sortedArray);
+    }
+    quickSort(){
+
+    }
+    bubbleSort(){
+
+    }
+    about(){
+        alert("Made by Varun Kedia\npurplevarun@gmail.com");
+    }
+    
+    //--------------------------------
     render(){
         const {a} = this.state;
         
@@ -34,11 +55,23 @@ export default class SortingVisualizer extends React.Component {
                     ))}
                 </div>
                 <div className="inside_wrap">
-                    <button className="btn btn-info btn-sm" onClick={()=>this.shuffleArray()}>Shuffle Array</button>
+                    <button className="btn btn-info btn-sm varunbtn" onClick={()=>this.shuffleArray()}>Shuffle Array</button> <br/>
+                    <button className="btn btn-success varunbtn" onClick={()=>this.mergeSort()}>Merge Sort</button>
+                    <button className="btn btn-success varunbtn" onClick={()=>this.quickSort()}>Quick Sort</button>
+                    <button className="btn btn-success varunbtn" onClick={()=>this.bubbleSort()}>Bubble Sort</button> <br/>
+                    <button className="btn btn-danger btn-sm varunbtn" onClick={()=>this.about()}>About</button> <br/>
                 </div>
             </div>
             
 
         );
     }
+    
+}
+function checkarrays(array1,array2) {
+    if(array1.length!==array2.length) return false;
+    for(let i=0;i<array1.length;i++) {
+        if(array1[i]!==array2[i]) return false;
+    }
+    return true;
 }
