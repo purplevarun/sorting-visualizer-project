f839c28a53ab91949a4d03aec9716ac4c608f94f
Varun Kedia <purplevarun@gmail.com>
2021-01-09 02:17:59 +0530

    merge sort aimation array debug

--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -33,5 +33,34 @@ export function merge_sort(array){
 function merge_sort_helper(main_array,l,r,aux_array,animations) {
     if(l==r) return;
     const m = Math.floor((l+r)/2);
-    
+    merge_sort_helper(main_array,l,m,aux_array,animations);
+    merge_sort_helper(main_array,m+1,r,aux_array,animations);
+    do_merge(main_array,l,m,r,aux_array,animations);
+}
+function do_merge(main_array,l,m,r,aux_array,animations) {
+    let k = l,i=l,j=m+1;
+    while(i<=m && j<=r){
+        animations.push([i,j]); // values being compared right now  // change color
+        animations.push([i,j]); // revert color
+        if(aux_array[i]<=aux_array[j]){
+            animations.push([k,aux_array[i]]); // overwriting value at index k 
+            main_array[k++] = aux_array[i++];
+        }
+        else{
+            animations.push([k,aux_array[j]]); // overwriting value at index k 
+            main_array[k++] = aux_array[j++];
+        }
+    }
+    while(i<=m){
+        animations.push([i,i]); // change color
+        animations.push([i,i]);  // revert color
+        animations.push([k,aux_array[i]]);
+        main_array[k++]=aux_array[i++];
+    }
+    while(j<=r){
+        animations.push([j,j]); // change color
+        animations.push([j,j]);  // revert color
+        animations.push([k,aux_array[j]]);
+        main_array[k++]=aux_array[j++];
+    }
 }
